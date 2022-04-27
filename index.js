a11b919551fbaaef9e5557791fcf2f9f182661e8
Nick Story <storynickolasl@gmail.com>
2022-04-27 07:29:49 -0400

    commit tree 5429874a92c3b590f62daf5234a67629d90fb724

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random()) * 3 + 1;
+
+
+let num3 = 94;
+let num4 = 9;
+
+let mod = num3 % num4;
+
+let max = Math.max(1, 2, 3, 5, 20);
